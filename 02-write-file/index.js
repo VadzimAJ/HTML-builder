d5545d2ff25095c6894d73ff62c74d31342b13d0
Pong-Kong <Janckman@gmail.com>
2023-05-08 23:58:13 +0300

    feat: completed part 2

---
+++ 02-write-file/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const filePath = path.join(__dirname, 'output.txt');
+const writeStream = fs.createWriteStream(filePath, { flags: 'a' });
+
+console.log('Введите текст. Для выхода введите "exit".');
+
+rl.on('line', (input) => {
+  if (input.toLowerCase() === 'exit') {
+    console.log('Программа завершена.');
+    process.exit();
+  }
+  writeStream.write(`${input}\n`);
+});
